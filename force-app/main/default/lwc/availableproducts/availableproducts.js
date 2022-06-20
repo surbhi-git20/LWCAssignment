1c2393f90e06cf1185f9c656017ccffbf3820ce9
Surbhi Gupta <surbhi.gupta@justeattakeaway.com>
2022-06-20 21:47:55 +0200

    LWCAssignment Changes V2

--- force-app/main/default/lwc/availableproducts/availableproducts.js
+++ force-app/main/default/lwc/availableproducts/availableproducts.js
@@ -1,8 +1,19 @@
 import { LightningElement ,api, wire, track} from 'lwc';
 import getProductList from '@salesforce/apex/getProductDetails.getProductList';
 import addtoOrder from '@salesforce/apex/orderProductHandler.addtoOrder';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import {publish,subscribe, unsubscribe,APPLICATION_SCOPE,MessageContext} from "lightning/messageService";
+import SAMPLEMC from "@salesforce/messageChannel/custommessageChannel__c";
 
 export default class LightningDatatableLWCExample extends LightningElement {
+    @track error;
+    @track prodList ;
+    wiredpricebookentryList = [];
+    disableButton= false;
+    subscription = null;
+    @wire(MessageContext)
+    messageContext;
+    displaySpinner = false;
     @track columns = [{
             label: 'Product name',
             fieldName: 'Name',
@@ -16,32 +27,55 @@ export default class LightningDatatableLWCExample extends LightningElement {
             sortable: true
         }
     ];
- 
-    @track error;
-    @track prodList ;
-    @api recordId;
+
     @wire(getProductList)
-    wiredPricebookEntry({
-        error,
-        data
-    }) {
-        if (data) {
-            let tempRecords = JSON.parse( JSON.stringify( data ) );
+    wiredPricebookEntry(result) 
+    {
+        this.wiredpricebookentryList = result;
+        if (result.data) {
+            let tempRecords = JSON.parse( JSON.stringify( result.data ) );
             tempRecords = tempRecords.map( row => {
                 return { Name: row.Product2.Name, UnitPrice: row.UnitPrice, pbookid: row.Id};
             })
             this.prodList = tempRecords;
-        } else if (error) {
-            this.error = error;
+        } else if (result.error) {
+            this.error = result.error;
         }
     }
+    connectedCallback(){
+        this.subscribeMC();
+    }
 
+    subscribeMC() {
+        if (this.subscription) {
+          return;
+        }
+        this.subscription = subscribe(
+          this.messageContext,
+          SAMPLEMC,
+          message => {
+            if (message.source == "orderproduct") {
+                console.log('listener2' ,message);
+                this.disableButton = true;
+            } 
+          },
+          { scope: APPLICATION_SCOPE }
+        );
+    }
+    unsubscribeMC() {
+        unsubscribe(this.subscription);
+        this.subscription = null;
+    }
+
+    disconnectedCallback()
+    {
+        this.unsubscribeMC();
+    }
     handleclick() {
         this.displaySpinner = true;
         var selectedRecords =  this.template.querySelector("lightning-datatable").getSelectedRows();
         if(selectedRecords.length > 0){
-            console.log('selectedRecords are ', selectedRecords);
-   
+
             let ids = '';
             selectedRecords.forEach(currentItem => {
                 ids = ids + ',' + currentItem.pbookid;
@@ -55,8 +89,13 @@ export default class LightningDatatableLWCExample extends LightningElement {
             .then(result => {
                 if(result == 'Success') {
                     eval("$A.get('e.force:refreshView').fire();");
-                    const selectedEvent = new CustomEvent('added', { detail: this.recordId });
-                    this.dispatchEvent(selectedEvent);
+                    this.template.querySelector('lightning-datatable').selectedRows = [];
+                    const message = {
+                        message : "This is simple message from LWC",
+                        source : "availableproduct"
+                    };
+                    publish(this.messageContext, SAMPLEMC, message);
+                    this.displaySpinner = false;
                 }
 
             })
--- force-app/main/default/lwc/orderProduct/orderProduct.js
+++ force-app/main/default/lwc/orderProduct/orderProduct.js
@@ -1,8 +1,20 @@
 import { LightningElement ,api, wire, track} from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import getOrderProductList from '@salesforce/apex/orderProductHandler.getOrderProductList';
 import activateOrder from '@salesforce/apex/orderProductHandler.activateOrder';
+import {publish,subscribe, unsubscribe,APPLICATION_SCOPE,MessageContext} from "lightning/messageService";
+import SAMPLEMC from "@salesforce/messageChannel/custommessageChannel__c";
+import { refreshApex } from '@salesforce/apex';
 
 export default class LightningDatatableLWCExample extends LightningElement {
+    subscription = null;
+    @wire(MessageContext)
+    messageContext;
+    @track error;
+    @api orderprodList ;
+    displaySpinner = false;
+    @track currentorderId;  
+    wiredorderitemList = [];
     @track columns = [{
             label: 'Product name',
             fieldName: 'Name',
@@ -29,27 +41,56 @@ export default class LightningDatatableLWCExample extends LightningElement {
         },
 
     ];
- 
-    @track error;
-    @track orderprodList ;
-    @track currentorderId;    
+  
     @wire(getOrderProductList)
-    wiredOrderitems({
-        error,
-        data
-    }) {
-        if (data) {
+    wiredOrderitems(
+        result
+    )
+     {
+        this.wiredorderitemList = result;
+        if (result.data) {
 
-            let tempRecords = JSON.parse( JSON.stringify( data ) );
+            let tempRecords = JSON.parse( JSON.stringify( result.data ) );
             tempRecords = tempRecords.map( row => {
                 return { Name: row.Product2.Name, UnitPrice: row.UnitPrice, Quantity: row.Quantity, ItemTotalPrice:row.TotalPrice,orderrecord: row.OrderId};
             })
             this.orderprodList = tempRecords;
-            this.currentorderId =tempRecords[0].orderrecord;
-        } else if (error) {
-            this.error = error;
+            this.currentorderId =tempRecords && tempRecords.length >0? tempRecords[0].orderrecord : '';
+        } else if (result.error) {
+            this.error = result.error;
         }
     }
+    connectedCallback(){
+        this.subscribeMC();
+    }
+
+    subscribeMC() {
+        if (this.subscription) {
+          return;
+        }
+        this.subscription = subscribe(
+          this.messageContext,
+          SAMPLEMC,
+          message => {
+            console.log('message' ,message);
+            if (message.source == "availableproduct") {
+                console.log('listener' ,message);
+                refreshApex(this.wiredorderitemList);
+            } 
+            //this.handleMessage(message);
+          },
+          { scope: APPLICATION_SCOPE }
+        );
+      }
+      unsubscribeMC() {
+        unsubscribe(this.subscription);
+        this.subscription = null;
+      }
+
+      disconnectedCallback()
+      {
+        this.unsubscribeMC();
+      }
 
     handleclick(){
         this.displaySpinner = true;
@@ -57,9 +98,14 @@ export default class LightningDatatableLWCExample extends LightningElement {
             orderId: this.currentorderId,
         })
         .then(result => {
+            const message = {
+                message : "This is simple message from orderproduct",
+                source:"orderproduct"
+            };
             this.displaySpinner = false;
-            this.showCategories = false;
-            this.showFinish = true;
+            publish(this.messageContext, SAMPLEMC, message);
+            eval("$A.get('e.force:refreshView').fire();") 
+            refreshApex(this.wiredorderitemList);
         })
         .catch((error) => {
             const evt = new ShowToastEvent({
@@ -68,7 +114,7 @@ export default class LightningDatatableLWCExample extends LightningElement {
                             variant: 'error',
                             mode: 'dismissable'
                         });
-           // this.dispatchEvent(evt);
+            this.dispatchEvent(evt);
             this.displaySpinner = false;
         });
     }
